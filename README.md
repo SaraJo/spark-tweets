12e5b3bc567d2770b83a43fe485624c29404ed37
sarajo <sarajchipps@gmail.com>
2013-12-09 16:32:29 -0500

    made some changes, removed token

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+#Spark Tweets
+##Repo to Tweet from your Spark Core arduino in C
 Get a token to tweet from your arduino at 'http://arduino-tweet.appspot.com/' in order to use this code with your Spark Core. 
--- tweet.cpp
+++ tweet.cpp
@@ -19,10 +19,10 @@ void setup()
     client.println("POST /update HTTP/1.0");
     client.println("Host: " LIB_DOMAIN);
     client.print("Content-Length: ");
-    client.println(strlen(msg)+strlen("2236980362-RkQyo0VdfOKK3LgvZTRabeRHbH5r8eJ9IVck7bX")+14);
+    client.println(strlen(msg)+strlen(**PLACE TOKEN HERE**)+14);
     client.println();
     client.print("token=");
-    client.print("2236980362-RkQyo0VdfOKK3LgvZTRabeRHbH5r8eJ9IVck7bX");
+    client.print(**PLACE TOKEN HERE**);
     client.print("&status=");
     client.println(msg);
 }
